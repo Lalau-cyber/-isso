bac92f60d2838937ea31bba3c205c8f417467c12
Laura <laurinha.ss1305@gmail.com>
2025-06-09 15:21:04 -0300

    Add files via upload

---
+++ frutas.js
@@ -0,0 +1,14 @@
+const frutas = ["laranja"]
+console.log(frutas)
+
+function adicionarFinal() {
+   
+      fruta = prompt("digite uma fruta")
+      frutas.push(fruta)
+      console.log(frutas)
+}
+function removerFinal(){
+
+    frutas.pop(fruta)
+    console.log(frutas)
+}
