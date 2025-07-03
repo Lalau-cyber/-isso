8bc132de122e5f8ef881ba8440c6f814fcb7f7c8
Laura <laurinha.ss1305@gmail.com>
2025-07-03 15:20:07 -0300

    Add files via upload

--- daniela22.js
+++ daniela22.js
@@ -1,14 +1,13 @@
 function Calcular(){
-var valor1 = document.getElementById('entrada1') // ou pode botar o .value aqui
-var valor2 = document.getElementById('entrada2')
-var res = document.getElementById('resultado')
-
-var resultado = Number( valor1.value )+ Number(valor2.value)
-
-res.innerHTML = `o resultado da operação é : ${resultado} `
-
+    var valor1 = document.getElementById('entrada1') // ou pode botar o .value aqui
+    var valor2 = document.getElementById('entrada2')
+    var res = document.getElementById('resultado')
+    
+    var resultado = Number( valor1.value )+ Number(valor2.value)
+    
+ res.innerHTML = `o resultado da operação é : ${resultado} `
+    
 }
-
-
-
-
+   
+    
+    
