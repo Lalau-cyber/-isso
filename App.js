88a0a4bdcb2cd16019172e4bffd4fbb566263c07
Laura <laurinha.ss1305@gmail.com>
2025-09-09 17:04:56 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,41 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TouchableOpacity, Alert } from 'react-native';
 
 export default function App() {
+
+  function handleButtonPress() {
+    Alert.alert(`Botao pressionado`, `voce clicou no botao`)
+  }
+
+  function handleTouchablePress() {
+    Alert.alert(`Touchable pressionado`, `voce clicou no botao personalizado`)
+  }
+
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.texto}></Text>
-      <Text style={styles.letra}></Text>
-      <Saudacao texto = "ola " />
-      <MeuNome texto = "Laura"/>
+      <Text style={styles.title}>Ola mundinho</Text>
+      <Image source={{ uri: 'https://images.unsplash.com/photo-1610393144490-a930182ad2f1?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG9yaXpvbnRlJTIwbWFyfGVufDB8fDB8fHww' }}
+        style={styles.image}
+      />
+      <Button title='clique aqui'
+        onpress={handleButtonPress}
+        style={styles.customButtom} />
+      <TouchableOpacity style={styles.customButtom}
+        onPress={handleTouchablePress}>
+        <Text style={styles.buttonText}> uhuh</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-function Saudacao(props){
-  return(
-    <Text style={styles.texto}>{props.texto}</Text>
-
-  )
-}
-function MeuNome(props){
-  return(
-    <Text style={styles.texto}>{props.texto}</Text>
-
-  )
-}
-
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
+    backgroundColor: 'rgb(220, 250, 252)',
+    alingItems: 'center',
     justifyContent: 'center',
-    
+    padding: 20,
   },
-  texto:{
-    fontSize: 30,
-    color: `white`
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20
   },
 
-});
+  image: {
+    width: 190,
+    height: 190,
+    marginBottom: 40
+  },
+
+  customButtom: {
+    backgroundColor: 'rgb(12, 164, 211)',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontSize: 16
+  }
+
+
+
+})
