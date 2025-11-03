4a2c0c4ade7dc192b26f36f329c1149438c51304
Laura <laurinha.ss1305@gmail.com>
2025-11-03 16:07:22 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,42 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+const Stack = createStackNavigator()
+
+function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.titulo}>olalaa!</Text>  
-        <Image source = {{uri: 'https://static.vecteezy.com/ti/vetor-gratis/t1/7384234-fundo-ceu-azul-e-nuvens-brancas-vetor.jpg'}}
-      style={{width:300, 
-        height: 300, 
-        borderColor:'black', 
-        borderWidht:1,
-        alignItems: 'center',
-        justifyContent: 'top',
-        margin: 100,
-      }
-      }/>
-      <Button style={styles.batao} title='Clique ' onPress={()=>alert("você clicou")}/>
-      <Button style={styles.batao} title=' aqui' onPress={()=>alert("de novo")}/>
+      <Text>Você é Aluno?</Text>
+      <Button title="Sim" onPress={() => navigation.navigate('SimScreen')} />
+      <Button title="Não" onPress={() => navigation.navigate('NaoScreen')} />
     </View>
   );
 }
+
+function SimScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Bem-vindo, Aluno!</Text>
+    </View>
+  );
+}
+
+function NaoScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Você é o administrador?</Text>
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="SimScreen" component={SimScreen} />
+        <Stack.Screen name="NaoScreen" component={NaoScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'top',
-    margin: 100,
-    
-  },
-  batao: {
-   flex: 1,
-    flexDirection: 'row',
     justifyContent: 'center',
-   alignItems: 'center'
-
-  }
-
-  
- 
+    alignItems: 'center',
+  },
 });
